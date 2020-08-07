ff140242489160b239db2901a6775fb2d9d1dc60
Arda Serim <arserim20@gmail.com>
2020-08-07 23:52:34 +0300

    Initial Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,223 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerController : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerController"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""15b7a6fd-623d-4c0f-ae9e-84bace22b92f"",
+            ""actions"": [
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""6ed4c9b3-c33d-477f-bbd4-487fab2f34dd"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Slide"",
+                    ""type"": ""Button"",
+                    ""id"": ""a955759f-8c2d-40e4-9a86-5656e65f2e73"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack"",
+                    ""type"": ""Button"",
+                    ""id"": ""6a7140e9-a1e6-49ac-a418-79b778c4c915"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""fc5f5b3e-f43c-4a9e-8715-92f16818258a"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": ""Hold(duration=0.3)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a874a80b-de4f-4833-8aa4-0a02cc3add01"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": ""Hold(duration=0.3)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""72e183d6-1560-4d20-a44e-ba711e3fbbf4"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Slide"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""eeea8f8a-1958-4a7c-a246-3a54ff78470b"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Slide"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""74e136db-e787-4169-91cd-2b2aa059a2e2"",
+                    ""path"": ""<Gamepad>/buttonWest"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4dd697b9-b403-4872-b694-cf5cd586b484"",
+                    ""path"": ""<Keyboard>/ctrl"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Jump = m_Gameplay.FindAction("Jump", throwIfNotFound: true);
+        m_Gameplay_Slide = m_Gameplay.FindAction("Slide", throwIfNotFound: true);
+        m_Gameplay_Attack = m_Gameplay.FindAction("Attack", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Jump;
+    private readonly InputAction m_Gameplay_Slide;
+    private readonly InputAction m_Gameplay_Attack;
+    public struct GameplayActions
+    {
+        private @PlayerController m_Wrapper;
+        public GameplayActions(@PlayerController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Jump => m_Wrapper.m_Gameplay_Jump;
+        public InputAction @Slide => m_Wrapper.m_Gameplay_Slide;
+        public InputAction @Attack => m_Wrapper.m_Gameplay_Attack;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Jump.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Slide.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnSlide;
+                @Slide.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnSlide;
+                @Slide.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnSlide;
+                @Attack.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnAttack;
+                @Attack.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnAttack;
+                @Attack.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnAttack;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Slide.started += instance.OnSlide;
+                @Slide.performed += instance.OnSlide;
+                @Slide.canceled += instance.OnSlide;
+                @Attack.started += instance.OnAttack;
+                @Attack.performed += instance.OnAttack;
+                @Attack.canceled += instance.OnAttack;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnJump(InputAction.CallbackContext context);
+        void OnSlide(InputAction.CallbackContext context);
+        void OnAttack(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/Ninja.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja : MonoBehaviour
+{
+    int speed;
+    int score;
+    [SerializeField] float stamina;
+
+    Rigidbody2D rb;
+    Animator animator;
+    PlayerController playerController;
+    EdgeCollider2D edgeCollider2D;
+
+    [SerializeField]bool isJumping;
+    [SerializeField]bool onGround;
+    void Awake()
+    {
+        speed = 2;
+        score = 0;
+        stamina = 100;
+
+        playerController = new PlayerController();
+        animator = gameObject.GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        edgeCollider2D = gameObject.GetComponent<EdgeCollider2D>();
+
+        isJumping = false;
+
+        playerController.Gameplay.Jump.started += ctx => { isJumping = true; };
+        playerController.Gameplay.Jump.performed += ctx => { isJumping = false; onGround = false; };
+        playerController.Gameplay.Jump.canceled += ctx => { isJumping = false; onGround = false; };
+
+        playerController.Gameplay.Slide.performed += ctx => SlideCaller();
+
+        playerController.Gameplay.Attack.performed += ctx => AttackCaller();
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector3(speed, rb.velocity.y);
+
+        Jump();
+        StaminaRegenarater();
+    }
+
+    /// <summary>
+    /// Jump depending on how long press Jump Key (If do not stop pressing, stop automaticly)
+    /// </summary>
+    void Jump()
+    {
+        if (isJumping && onGround && stamina > 0.5)
+        {
+            stamina -= 0.125f;
+            rb.velocity = new Vector2(rb.velocity.x, 7);
+        }
+    }
+
+    /// <summary>
+    /// Make the "Slide Animation"
+    /// </summary>
+    IEnumerator Slide()
+    {
+        if (!animator.GetBool("Slide") && stamina > 10)
+        {
+            stamina -= 10;
+            animator.SetBool("Slide", true);
+            yield return new WaitForSeconds(0.75f);
+            animator.SetBool("Slide", false);
+        }
+    }
+
+    /// <summary>
+    /// Just call Slide()
+    /// </summary>
+    void SlideCaller()
+    {
+        StartCoroutine(Slide());
+    }
+
+    /// <summary>
+    /// Make the "Attack Animation" and decrease stamina everytime
+    /// </summary>
+    IEnumerator Attack()
+    {
+        if (!animator.GetBool("Attack") && stamina > 20)
+        {
+            stamina -= 20;
+            animator.SetBool("Attack", true);
+            yield return new WaitForSeconds(0.75f);
+            animator.SetBool("Attack", false);
+        }
+    }
+
+    /// <summary>
+    /// Just call Attack()
+    /// </summary>
+    void AttackCaller()
+    {
+        StartCoroutine(Attack());
+    }
+
+    /// <summary>
+    /// Regenerate stamina when doing nothing require stamina
+    /// </summary>
+    void StaminaRegenarater()
+    {
+        if (stamina <= 99.585f && !animator.GetBool("Attack") && onGround && !animator.GetBool("Slide"))
+        {
+            stamina += 0.415f;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Ground");
+            onGround = true;
+        }
+    }
+
+    #region OnEnable / OnDisable
+    private void OnEnable()
+    {
+        playerController.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerController.Disable();
+    }
+    #endregion
+}
